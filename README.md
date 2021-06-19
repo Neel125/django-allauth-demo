935b4a91b341392b2df415aa7cf9e42558b89996
Neel Gajjar <neelgajjar125@gmail.com>
2021-06-19 14:47:48 +0530

    initial setup of google auth

--- README.md
+++
@@ -1,2 +0,0 @@
-# django-allauth-demo
-django authentication with google and facebook
---
+++ social_authentication/social_auth/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.HomePage.as_view(), name="blog_index"),
+]
+
+
